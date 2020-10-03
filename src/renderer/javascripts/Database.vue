18097549337142dc22dd713badbdfd0977f28307
Daniel Smith <jellymann@gmail.com>
2020-10-04 01:02:54 +0200

    add: select table from sidebar

--- src/renderer/javascripts/Database.vue
+++ src/renderer/javascripts/Database.vue
@@ -5,13 +5,23 @@
     <div class="main">
       <div class="left">
         <div v-if="loadingTables">Loading...</div>
-        <ul v-else>
-          <li v-for="table in tables" :key="table.name">
-            {{ table.name }}
+        <ul v-else class="tables">
+          <li
+            v-for="table in tables"
+            :key="table.name"
+            :class="{ 'tables__table': true, 'tables__table--current': table === currentTable }"
+          >
+            <a @click.prevent="selectTable(table)" class="tables__table-link">
+              {{ table.name }}
+            </a>
           </li>
         </ul>
       </div>
-      <div class="content"></div>
+      <div class="content">
+        <div v-if="currentTable">
+          {{currentTable.name}}
+        </div>
+      </div>
       <div class="right"></div>
     </div>
   </div>
@@ -50,6 +60,28 @@
 .content {
   flex: 1 0 auto;
 }
+
+.tables {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  &__table {
+    cursor: pointer;
+    margin: 0.5rem 0;
+    
+    &:hover, &--current {
+      background-color: $highlight-background;
+      color: $highlight-foreground;
+    }
+  }
+
+  &__table-link {
+    display: block;
+
+    padding: 0.5rem 1rem;
+  }
+}
 </style>
 
 <script>
@@ -66,6 +98,8 @@ export default {
     const loadingTables = ref(true);
     const tables = ref([]);
 
+    const currentTable = ref(null);
+
     callMain('fetchTables', { connectionId: props.id })
       .then(resultTables => {
         tables.value = resultTables;
@@ -78,9 +112,15 @@ export default {
         loadingTables.value = false;
       });
 
+    const selectTable = (table) => {
+      currentTable.value = table;
+    }
+
     return {
       loadingTables,
-      tables
+      tables,
+      selectTable,
+      currentTable
     };
   }
 }
