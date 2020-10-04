7af90e1ca1d6af00d18298b5e9218807c593d0b3
Daniel Smith <jellymann@gmail.com>
2020-10-04 12:45:40 +0200

    add: styling for connection view

--- src/renderer/javascripts/Connection.vue
+++ src/renderer/javascripts/Connection.vue
@@ -1,34 +1,113 @@
 <template>
-  <h1>Hello, vue + electon!</h1>
-  <div>
-    <label>Host</label>
-    <input v-model="connection.host" />
+  <div class="center">
+    <div class="form">
+      <div class="form__row">
+        <div class="form__field">
+          <label class="form__label">Host</label>
+          <input class="form__input" v-model="connection.host" />
+        </div>
+        <div class="form__field form__field--short">
+          <label class="form__label">Port</label>
+          <input class="form__input" v-model="connection.port" />
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__field">
+          <label class="form__label">Username</label>
+          <input class="form__input" v-model="connection.username" />
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__field">
+          <label class="form__label">Password</label>
+          <input class="form__input" v-model="connection.password" type="password" />
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__field">
+          <label class="form__label">Database</label>
+          <input class="form__input" v-model="connection.database" />
+        </div>
+      </div>
+      <div class="form__actions">
+        <div v-if="error" class="form__error">
+          {{ error }}
+        </div>
+        <button class="form__action" @click="onButtonClick()">Click me!</button>
+      </div>
+    </div>
   </div>
-  <div>
-    <label>Port</label>
-    <input v-model="connection.port" />
-  </div>
-  <div>
-    <label>Username</label>
-    <input v-model="connection.username" />
-  </div>
-  <div>
-    <label>Password</label>
-    <input v-model="connection.password" type="password" />
-  </div>
-  <div>
-    <label>Database</label>
-    <input v-model="connection.database" />
-  </div>
-  <div v-if="error" class="error">
-    {{ error }}
-  </div>
-  <button @click="onButtonClick()">Click me!</button>
 </template>
 
-<style scoped>
-.error {
-  color: red;
+<style lang="scss" scoped>
+.center {
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 1rem;
+  max-width: 32rem;
+  flex: 1 0 auto;
+
+  &__row {
+    display: flex;
+    margin-bottom: 0.5rem;
+  }
+
+  &__field {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 auto;
+
+    &--short {
+      max-width: 4.25rem;
+    }
+
+    & + & {
+      margin-left: 1rem;
+    }
+  }
+
+  &__label {
+    font-weight: bold;
+    margin-bottom: 0.25rem;
+  }
+
+  &__input {
+    padding: 0.5rem 1rem;
+    border-radius: $border-radius;
+    border: $input-border;
+  }
+
+  &__actions {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top: 1rem;
+  }
+  
+  &__error {
+    color: red;
+    margin-right: 1rem;
+  }
+
+  &__action {
+    border-radius: $border-radius;
+    background: $button-background;
+    border: $button-border;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+
+    &:hover {
+      background: $button-background-hover;
+    }
+  }
 }
 </style>
 
