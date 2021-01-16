88ffa0a9db01d8adaf0a06c6ba75463d34415104
Daniel Smith <jellymann@gmail.com>
2021-01-16 02:20:24 +0200

    remove: previous performance hack

--- src/renderer/javascripts/TableRow.vue
+++ src/renderer/javascripts/TableRow.vue
@@ -45,7 +45,6 @@
     width: 100%;
     height: 100%;
     align-items: center;
-    position: relative;
   }
 
   &__value {
@@ -63,11 +62,6 @@
     height: 100%;
     border: 0;
     resize: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: calc(3rem - 3px);
 
     &::-webkit-scrollbar {
       display: none;
